7c1fdc3eaf216ed26e52f61b07da6edd65bfb5fb
firasel <mdrasel601390@gmail.com>
2022-08-03 21:40:13 +0600

    social media meta tag added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,22 @@ const HomePage: NextPage = () => {
           name="description"
           content="Hi, I'm Fi Rasel, A Programmer and MERN stack developer."
         />
+        <meta property="og:title" content="Fi Rasel - MERN Stack Developer" />
+        <meta
+          property="og:description"
+          content="Hi, I'm Fi Rasel, A Programmer and MERN stack developer."
+        />
+        <meta property="og:image" content="https://i.ibb.co/2SDhrHX/og.png" />
+        <meta property="og:url" content="https://terminal.firasel.com/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Fi Rasel - MERN Stack Developer" />
+        <meta name="twitter:url" content="https://terminal.firasel.com/" />
+        <meta
+          name="twitter:description"
+          content="Hi, I'm Fi Rasel, A Programmer and MERN stack developer."
+        />
+        <meta name="twitter:image" content="https://i.ibb.co/2SDhrHX/og.png" />
       </Head>
       <main>
         <Home />
