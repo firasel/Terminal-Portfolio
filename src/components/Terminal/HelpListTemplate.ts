ddd562830e43a60d8869614ebafcfc636c7182a0
ayencha <ijsjaaa@gmail.com>
2024-11-16 21:35:24 +0100

    Update HelpListTemplate.ts

--- src/components/Terminal/HelpListTemplate.ts
+++ src/components/Terminal/HelpListTemplate.ts
@@ -7,9 +7,6 @@ const helpListTemplate = () => {
 <pre class="whitespace-pre-line md:whitespace-pre-wrap">
 <span class="text-cyan">about</span>    - about me
 <span class="text-cyan">skills</span>   - my skills
-<span class="text-cyan">projects</span> - my project list
-<span class="text-cyan">project projectNumber</span>,
-<span class="text-cyan">pr projectNumber</span> - project details
 <span class="text-cyan">social</span>   - social account link
 <span class="text-cyan">contact</span>  - contact information
 <span class="text-cyan">help</span>     - all available command list
