d268e70af4bc3f47e61ddba80423a1a720a2aeb3
ayencha <ijsjaaa@gmail.com>
2024-11-16 20:38:58 +0100

    Update CommandTemplate.ts

--- src/components/Terminal/CommandTemplate.ts
+++ src/components/Terminal/CommandTemplate.ts
@@ -3,7 +3,7 @@ const commandTemplate = (cmd: string) => {
   parentDiv.className = "w-full flex";
   parentDiv.innerHTML = `
   <span class="text-green text-lg">
-    <span class="text-orange-default">visitor</span>@terminal.firasel.com:~$
+    <span class="text-orange-default">visitor</span>@terminal.ayencha.com:~$
     <br class="md:hidden" />
     <span class="text-white text-lg md:hidden">&gt;</span>
     <span class="text-cyan">${cmd}</span>
