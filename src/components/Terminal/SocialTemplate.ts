746b1f8fded5255b189072639b8c49b5bdbca486
ayencha <ijsjaaa@gmail.com>
2024-11-21 19:23:28 +0000

    fa

--- src/components/Terminal/SocialTemplate.ts
+++ src/components/Terminal/SocialTemplate.ts
@@ -25,16 +25,6 @@ const socialTemplate = () => {
   >
     LinkedIn
   </a>
-  <br />
-  &gt;
-  <a
-    href="https://www.facebook.com/ayenchaaa"
-    target="_blank"
-    rel="noreferrer"
-    class="text-emerald-400 pl-1"
-  >
-    Facebook
-  </a>
     `;
   return parentDiv;
 };
