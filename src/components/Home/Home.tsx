cc19522cddf1351c1f3a296967be2b7ab8b6503b
firasel2 <rasel@lemonhive.com>
2025-11-05 00:10:13 +0600

    refactor: simplify Home component signature by removing NextPage import and annotation. This removes an unnecessary dependency on Next.js types within the component.

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,8 @@
-import { NextPage } from "next";
 import SoundControl from "../SoundControl/SoundControl";
 import Terminal from "../Terminal/Terminal";
 import TextDrop from "../TextDrop/TextDrop";
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <div className="bg-black min-w-full min-h-screen">
       <div className="min-w-full min-h-screen flex justify-center items-center overflow-hidden relative z-10">
