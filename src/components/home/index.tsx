dfec3a6aecac788c7062f2ef7df00cad630e3567
firasel2 <rasel@lemonhive.com>
2026-01-27 21:54:03 +0600

    feat: add TextDrop component to Home for enhanced visual effects

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,11 +1,13 @@
 import { LiquidBackground } from "../liquid-background";
 import { SoundControl } from "../sound-control";
 import { Terminal } from "../terminal";
+import { TextDrop } from "../text-drop";
 
 export function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4 md:p-8 font-mono">
       <LiquidBackground />
+      <TextDrop />
       <Terminal />
       <SoundControl />
     </div>
