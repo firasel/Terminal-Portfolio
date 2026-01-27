59fae6b51b42a617338b4345aa0f9eb250010c03
firasel2 <rasel@lemonhive.com>
2026-01-27 20:56:26 +0600

    feat: add index file for terminal templates to streamline exports and enhance modularity

---
+++ src/components/Terminal/templates/index.ts
@@ -0,0 +1,9 @@
+export { AboutTemplate } from "./about";
+export { CommandTemplate } from "./command";
+export { ContactTemplate } from "./contact";
+export { ErrorTemplate } from "./error";
+export { HelpListTemplate } from "./help-list";
+export { ProjectDetailsTemplate } from "./project-details";
+export { ProjectsTemplate } from "./projects";
+export { SkillsTemplate } from "./skills";
+export { SocialTemplate } from "./social";
